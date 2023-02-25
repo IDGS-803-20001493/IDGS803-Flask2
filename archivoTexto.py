f4cb232aa0356e1ba0c8bb1ca8305de02dded626
IDGS-803-20001493 <20001493@alumnos.utleon.edu.mx>
2023-02-25 13:02:45 -0600

    Trabajo de la clase 25 de febrero del 2023

---
+++ archivoTexto.py
@@ -0,0 +1,29 @@
+
+"""f=open('alumnos.txt','r')
+nombre = f.read()
+print(nombre)
+
+print("-----------------------------------")
+f.seek(9)
+
+nombre2=f.read()
+print(nombre2)
+
+f.close()"""
+
+"""f=open('alumnos.txt','r')
+nombre = f.readlines()
+
+for item in nombre:
+    print(item,end='')
+    
+f.close()"""
+
+"""
+f=open('alumnos.txt','w')
+f.write("hola mundo")
+f.close()"""  
+"""
+f=open('alumnos.txt','a')
+f.write("\n" + "hola mundo v2")
+f.close()"""
--- forms.py
+++ forms.py
@@ -3,9 +3,39 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField,FieldList, FormField, SelectField, RadioField
 
 from wtforms.fields import EmailField
+from wtforms import validators
+
+
+def mi_validacion(form,field):
+    if len(field.data) == 0:
+        raise validators.ValidationError('El campo no tiene datos')
 
 class UserForm(Form):
-    matricula=StringField('Matricula')
-    nombre = StringField('Nombre')
-    apaterno = StringField('Apaterno')
+    matricula=StringField('Matricula',[
+        validators.data_required(message='el campos es requerido'),
+        validators.length(min=4,max=15,message='no cumple con la longitud del campo')
+        ])
+    nombre = StringField('Nombre',[validators.data_required(message='el campos es requerido')])
+    apaterno = StringField('Apaterno', [mi_validacion])
     email=EmailField('Correo')
+
+class LoginForm(Form):
+    username=StringField('usuario',[
+        validators.data_required(message='el campos es requerido'),
+        validators.length(min=4,max=15,message='no cumple con la longitud del campo')
+        ])
+    password = StringField('password',[
+        validators.data_required(message='el campos es requerido'),
+        validators.length(min=4,max=15,message='no cumple con la longitud del campo')
+        ])
+
+
+class DiccionarioForm(Form):
+    ingles=StringField('ingles',[
+        validators.data_required(message='el campos es requerido'),
+        validators.length(min=4,max=15,message='no cumple con la longitud del campo')
+        ])
+    español = StringField('español',[
+        validators.data_required(message='el campos es requerido'),
+        validators.length(min=4,max=15,message='no cumple con la longitud del campo')
+        ])
